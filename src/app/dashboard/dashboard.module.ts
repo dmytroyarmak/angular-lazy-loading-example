dacff9623cf2ada5711eabf6d007b4354a458c69
Dmytro Yarmak <dmytroyarmak@gmail.com>
2017-06-04 11:39:03 +0300

    Add Chart.js to Dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import 'chart.js';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ChartsModule,
     DashboardRoutingModule
   ],
   declarations: [
